39691eba7fc0de60fbe728c45b4289a41f0e1f95
iam-krishnavamsi <109576412+iam-krishnavamsi@users.noreply.github.com>
2026-02-04 23:19:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,13 @@
-Back to Apps
-do-backend
-Healthy
-DO
-BLR1
-default-blr1
-https://imlocal.site
-Add components
-Actions
-Overview
-Insights
-Activity
-Runtime Logs
-Console
-Networking
-Settings
-Private Network
-Private IP
+from flask import Flask
+import time
 
-10.122.0.6
+app = Flask(__name__)
 
-Datacenter region
+@app.route("/")
+def home():
+    return "OK - App is running"
 
-Bangalore
-
-Datacenter 1
-
-blr1
-
-VPC network
-
-default-blr1
-IP range: 
-10.122.0.0/20
-
-Edit network
-Public Network
-Dedicated Egress IPs
-Static Ingress IPs
-We provide two IPs to point DNS records at to direct traffic to your app. Addresses are shared with other users.
-
-Static ingress IPs
-
-162.159.140.98
-
-172.66.0.96
-
-Domains
-Access your app using the starter domain we provide, or connect your own custom domains.
-
-Add domain
-Domains
-Status
-do-backend-kcoaj.ondigitalocean.app
-Active
-imlocal.site
-PRIMARY
-Active
-www.imlocal.site
-Active
-HTTP redirect rules
-Redirect traffic from a domain or path in your app to a different URL.
-Add redirect rule
+@app.route("/timeout-test")
+def timeout_test():
+    time.sleep(120)  # 2 minutes
+    return "Done after 120 seconds"

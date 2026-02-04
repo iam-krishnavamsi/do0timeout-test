dad2da055be487c747fa05546fc06e1ef8ba1559
iam-krishnavamsi <109576412+iam-krishnavamsi@users.noreply.github.com>
2026-02-04 23:11:19 +0530

    Add initial content to app.py

---
+++ app.py
@@ -0,0 +1,63 @@
+Back to Apps
+do-backend
+Healthy
+DO
+BLR1
+default-blr1
+https://imlocal.site
+Add components
+Actions
+Overview
+Insights
+Activity
+Runtime Logs
+Console
+Networking
+Settings
+Private Network
+Private IP
+
+10.122.0.6
+
+Datacenter region
+
+Bangalore
+
+Datacenter 1
+
+blr1
+
+VPC network
+
+default-blr1
+IP range: 
+10.122.0.0/20
+
+Edit network
+Public Network
+Dedicated Egress IPs
+Static Ingress IPs
+We provide two IPs to point DNS records at to direct traffic to your app. Addresses are shared with other users.
+
+Static ingress IPs
+
+162.159.140.98
+
+172.66.0.96
+
+Domains
+Access your app using the starter domain we provide, or connect your own custom domains.
+
+Add domain
+Domains
+Status
+do-backend-kcoaj.ondigitalocean.app
+Active
+imlocal.site
+PRIMARY
+Active
+www.imlocal.site
+Active
+HTTP redirect rules
+Redirect traffic from a domain or path in your app to a different URL.
+Add redirect rule
